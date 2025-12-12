01e0f034c049cf77511c681d458c2bcfba6b5505
ProjectFuritsu <capillanesjohnfritz@gmail.com>
2025-12-12 23:31:02 +0800

    Create specialist.controller.js

---
+++ controller/specialist.controller.js
@@ -0,0 +1,8 @@
+import con from "../db/connection.js";
+
+
+export async function get_specialist_list(req, res, next) {}
+export async function get_specialist_info(req, res, next) {}
+export async function insert_specialist(req, res, next) {}
+export async function update_specialist(req, res, next) {}
+export async function delete_specialist(req, res, next) {}
